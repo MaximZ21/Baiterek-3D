1870a5517efef0c9f9388b0eefcbb470ea3c7ed2
MaximZ21 <razel.agressor.21@gmail.com>
2019-03-26 00:04:16 +0600

    c1

--- app/src/main/java/com/example/android/opengl/MainActivity.java
+++ app/src/main/java/com/example/android/opengl/MainActivity.java
@@ -5,16 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class MainActivity extends AppCompatActivity {
+
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
 
-    public void onClick3(View v){
-        Intent i = new Intent(this,OpenGLES20Activity.class);
+    public void onClick3(View v) {
+        Intent i = new Intent(this, OpenGLES20Activity.class);
+        startActivity(i);
+    }
+
+    public void onClick1(View v) {
+        Intent i = new Intent(this, InfoActivity.class);
         startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/android/opengl/StorageClass.java
@@ -0,0 +1,10 @@
+package com.example.android.opengl;
+
+public class StorageClass {
+
+
+
+    public StorageClass(){
+
+    }
+}
